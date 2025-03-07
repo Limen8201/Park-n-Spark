56848d483c5b751b140688a36fa4b072d38ea282
Michael Bejo <91372910+Limen8201@users.noreply.github.com>
2025-03-07 18:39:57 +0800

    Update -- Park-n-Spark DB.py

--- DataBase/-- Park-n-Spark DB.py
+++ DataBase/-- Park-n-Spark DB.py
@@ -5,36 +5,53 @@ cursor = conn.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS userInfo(
-        userID INTEGER PRIMARY KEY AUTOINCREMENT,
+        userID INTEGER PRIMARY KEY,
+        password TEXT NOT NULL,
         firstName TEXT NOT NULL,
         lastName TEXT NOT NULL,
         emailAddress TEXT NOT NULL,
-        carType TEXT NOT NULL
+        vehicleType TEXT NOT NULL,
+        vehicleModel TEXT NOT NULL
+    )
+''')
+
+cursor.execute('''
+    create table if not exists parkingSlot(
+        parkingSLotID INTEGER PRIMARY KEY AUTOINCREMENT,
+        parkingLocation TEXT NOT NULL,
+        isAvailable BOOLEAN NOT NULL
     )
 ''')
 
 cursor.execute('''
     create table if not exists parkingInfo(
         parkingID INTEGER PRIMARY KEY AUTOINCREMENT,
-        userID INTEGER NOT NULL,
-        parkingLocation TEXT NOT NULL,
+        parkingSlotID INTEGER N  OT NULL,
         parkingStartTime TEXT NOT NULL,
         parkingEndTime TEXT NOT NULL,
-        isAvailable BOOLEAN NOT NULL,
-        FOREIGN KEY (userID) REFERENCES userInfo(userID)
+        foreign key (parkingSlotID) references parkingSlot(parkingSlotID)
     )
 ''')
 
 cursor.execute('''
-    create table if not exists locationInfo(
-        locationID INTEGER PRIMARY KEY AUTOINCREMENT,
-        parkingID INTEGER NOT NULL,
+    create table if not exists userLocationInfo(
+        userLocationInfoID INTEGER PRIMARY KEY AUTOINCREMENT,
         userID INTEGER NOT NULL,
         locationName TEXT NOT NULL,
         latitude DECIMAL(8,6) NOT NULL,
         longitude DECIMAL(9,6) NOT NULL,
-        foreign key (parkingID) references parkingInfo(parkingID),
         foreign key (userID) references userInfo(userID)
+)
+''')
+
+cursor.execute('''
+    create table if not exists parkingLocationInfo(
+        parkingLocationID INTEGER PRIMARY KEY AUTOINCREMENT,
+        parkingID INTEGER NOT NULL,
+        locationName TEXT NOT NULL,
+        latitude DECIMAL(8,6) NOT NULL,
+        longitude DECIMAL(9,6) NOT NULL,
+        foreign key (parkingID) references parkingInfo(parkingID)
     )
 ''')
 
@@ -49,3 +66,4 @@ cursor.execute('''
         foreign key (parkingID) references parkingInfo(parkingID)
     )
 ''')
+
