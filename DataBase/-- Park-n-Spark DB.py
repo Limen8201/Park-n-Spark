847c77a7ca51dadcfe5696d0e1aca7d67f49c644
Michael Bejo <91372910+Limen8201@users.noreply.github.com>
2025-03-14 08:47:41 +0800

    Update -- Park-n-Spark DB.py

--- DataBase/-- Park-n-Spark DB.py
+++ DataBase/-- Park-n-Spark DB.py
@@ -18,7 +18,7 @@ cursor.execute('''
 cursor.execute('''
     create table if not exists parkingSlot(
         parkingSLotID INTEGER PRIMARY KEY AUTOINCREMENT,
-        parkingLocation TEXT NOT NULL,
+        parkingSlotName TEXT NOT NULL,
         isAvailable BOOLEAN NOT NULL
     )
 ''')
@@ -26,31 +26,21 @@ cursor.execute('''
 cursor.execute('''
     create table if not exists parkingInfo(
         parkingID INTEGER PRIMARY KEY AUTOINCREMENT,
-        parkingSlotID INTEGER N  OT NULL,
-        parkingStartTime TEXT NOT NULL,
-        parkingEndTime TEXT NOT NULL,
+        parkingSlotID INTEGER NOT NULL,
+        parkingSlotName TEXT NOT NULL,
+        hoursReserved INTEGER NOT NULL,
         foreign key (parkingSlotID) references parkingSlot(parkingSlotID)
     )
 ''')
 
 cursor.execute('''
-    create table if not exists userLocationInfo(
-        userLocationInfoID INTEGER PRIMARY KEY AUTOINCREMENT,
+    create table if not exists logHistory(
+        logID INTEGER PRIMARY KEY AUTOINCREMENT,
         userID INTEGER NOT NULL,
-        locationName TEXT NOT NULL,
-        latitude DECIMAL(8,6) NOT NULL,
-        longitude DECIMAL(9,6) NOT NULL,
-        foreign key (userID) references userInfo(userID)
-)
-''')
-
-cursor.execute('''
-    create table if not exists parkingLocationInfo(
-        parkingLocationID INTEGER PRIMARY KEY AUTOINCREMENT,
         parkingID INTEGER NOT NULL,
-        locationName TEXT NOT NULL,
-        latitude DECIMAL(8,6) NOT NULL,
-        longitude DECIMAL(9,6) NOT NULL,
+        parkingName TEXT NOT NULL,
+        hoursReserved INTEGER NOT NULL,
+        foreign key (userID) references userInfo(userID),
         foreign key (parkingID) references parkingInfo(parkingID)
     )
 ''')
@@ -60,10 +50,16 @@ cursor.execute('''
         paymentID INTEGER PRIMARY KEY AUTOINCREMENT,
         userID INTEGER NOT NULL,
         parkingID INTEGER NOT NULL,
+        parkingName TEXT NOT NULL,
         paymentAmount DECIMAL(5,2) NOT NULL,
         paymentDate TEXT NOT NULL,
         foreign key (userID) references userInfo(userID),
-        foreign key (parkingID) references parkingInfo(parkingID)
+        foreign key (parkingID) references parkingInfo(parkingID),
+        foreign key (parkingName) references parkingInfo(parkingName)
     )
 ''')
+
+
+
+
 
